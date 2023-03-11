76e8d9fc34f47eb814f3228d64e503d912d0182b
Santi <shernandiu@alumnos.unex.es>
2023-03-11 12:25:58 +0100

    Fortaleza contraseña función independiente

--- src/gui/password_screen.java
+++ src/gui/password_screen.java
@@ -10,11 +10,6 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class password_screen extends JDialog {
-	private static final double LOWER_POINTS = 10;
-	private static final double UPPER_POINTS = 15;
-	private static final double NUMBER_POINTS = 15;
-	private static final double PUNCT_POINTS = 20;
-
 	private JPasswordField passwordField1;
 	private JButton okButton;
 	private JButton cancelarButton;
@@ -37,18 +32,11 @@ public class password_screen extends JDialog {
 
 		pack();
 
-//		passwordField1.addKeyListener(new KeyAdapter() {
-//			@Override
-//			public void keyTyped(KeyEvent e) {
-//				super.keyTyped(e);
-//				check_strength();
-//			}
-//		});
 		passwordField1.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				super.keyReleased(e);
-				check_strength();
+				progressBar1.setValue(util.PasswordStrength.check_strength(passwordField1.getPassword()));
 			}
 		});
 		okButton.addActionListener(new ActionListener() {
@@ -74,35 +62,6 @@ public class password_screen extends JDialog {
 		new password_screen(null);
 	}
 
-	private void check_strength() {
-		String password = new String(passwordField1.getPassword());
-		int size = passwordField1.getPassword().length;
-
-
-		Pattern upper = Pattern.compile("\\p{Upper}");
-		Pattern lower = Pattern.compile("\\p{Lower}");
-		Pattern number = Pattern.compile("\\p{Digit}");
-		Pattern punct = Pattern.compile("\\p{Punct}");
-
-		int number_upper = (int) upper.matcher(password).results().count();
-		int number_lower = (int) lower.matcher(password).results().count();
-		int number_number = (int) number.matcher(password).results().count();
-		int number_punct = (int) punct.matcher(password).results().count();
-
-		int strength = (int) (number_lower * LOWER_POINTS / 10 + (number_lower > 0 ? LOWER_POINTS : 0) +
-				number_upper * UPPER_POINTS / 10 + (number_upper > 0 ? UPPER_POINTS : 0) +
-				number_number * NUMBER_POINTS / 10 + (number_number > 0 ? NUMBER_POINTS : 0) +
-				number_punct * PUNCT_POINTS / 10 + (number_punct > 0 ? PUNCT_POINTS : 0));
-
-		progressBar1.setValue(strength);
-
-		System.out.println();
-		System.out.println("Password: " + password);
-		System.out.println("Lower: " + number_lower);
-		System.out.println("Upper: " + number_upper);
-		System.out.println("Numbers: " + number_number);
-		System.out.println("Punct: " + number_punct);
-	}
 
 	public char[] getPassword() {
 		return password;
---
+++ src/util/PasswordStrength.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.util.regex.Pattern;
+
+public class PasswordStrength {
+	private static final double LOWER_POINTS = 10;
+	private static final double UPPER_POINTS = 15;
+	private static final double NUMBER_POINTS = 15;
+	private static final double PUNCT_POINTS = 20;
+
+	private static final Pattern upper = Pattern.compile("\\p{Upper}");
+	private static final Pattern lower = Pattern.compile("\\p{Lower}");
+	private static final Pattern number = Pattern.compile("\\d");
+	private static final Pattern punct = Pattern.compile("\\p{Punct}");
+
+	public static int check_strength(char[] password_array) {
+		String password = new String(password_array);
+
+		int size = password_array.length;
+		
+		int number_upper = (int) upper.matcher(password).results().count();
+		int number_lower = (int) lower.matcher(password).results().count();
+		int number_number = (int) number.matcher(password).results().count();
+		int number_punct = (int) punct.matcher(password).results().count();
+
+		return (int) (number_lower * LOWER_POINTS / 10 + (number_lower > 0 ? LOWER_POINTS : 0) +
+				number_upper * UPPER_POINTS / 10 + (number_upper > 0 ? UPPER_POINTS : 0) +
+				number_number * NUMBER_POINTS / 10 + (number_number > 0 ? NUMBER_POINTS : 0) +
+				number_punct * PUNCT_POINTS / 10 + (number_punct > 0 ? PUNCT_POINTS : 0));
+	}
+}
